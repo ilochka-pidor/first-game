5094b6fb32f2f425afacca38fc110e2a6d62338d
ilochka-pidor <kurinzi50@gmail.com>
2025-11-29 14:17:29 +0100

    test

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
-my_list = [1, 2, 3]
-iterator = iter(my_list)
-print(iterator)
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    filename="logs.log",
+                    filemode="w",
+                    format="We have next logging message:%(asctime)s:%(levelname)s -%(message)s"
+                    )
+
+def adder(*args, **kwargs):
+    result = 0
+    for _ in args:
+        if type(_) == int or type(_) == bool or type(_) == float:
+            result += _
+        else:
+            try:
+                result+=float(_)
+                continue
+            except (ValueError, TypeError):
+                pass
+            try:
+                result+=int(_)
+                continue
+            except (ValueError, TypeError):
+                pass
+    for _ in kwargs.values():
+        if type(_) == int or type(_) == bool or type(_) == float:
+            result += _
+        else:
+            try:
+                result += float(_)
+                continue
+            except (ValueError, TypeError):
+                pass
+            try:
+                result += int(_)
+                continue
+            except (ValueError, TypeError):
+                pass
+    return result
